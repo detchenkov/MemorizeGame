084ea6aec5cfc4e28de98686bc9290675903fb8c
Ilia Detchenkov <detchenkov@bk.ru>
2020-11-14 00:25:17 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ilia Detchenkov on 14/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
